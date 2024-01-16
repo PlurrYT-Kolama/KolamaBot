03780a7a24ef4d5ae2708c9fac91e6331d5539ca
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-16 21:39:22 +0100

    Update ready.js

--- handles/events/ready.js
+++ handles/events/ready.js
@@ -8,6 +8,9 @@ module.exports = {
         ascii = `   ____        _                        _       \n  | __ )  ___ | |_   _ __ ___  __ _  __| |_   _ \n  |  _ \\ / _ \\| __| | '__/ _ \\/ _' |/ _' | | | |\n  | |_) | (_) | |_  | | |  __/ (_| | (_| | |_| |\n  |____/ \\___/ \\__| |_|  \\___|\\__,_|\\__,_|\\__, |\n                                          |___/ `
         console.log(`Logged in as ${client.user.tag}\nThis bot is in ${sCount} servers\n${ascii}\n\n`)
         client.user.setActivity(`Kolama.net`, { type: ActivityType.Watching })
+        if (client.test) {
+            process.exit(0)
+        }
         setInterval(function () {
             fs.writeFileSync('./times.json', '{\n    "Words": "of wisdom"\n}', 'utf-8')
         }, 86400000)
