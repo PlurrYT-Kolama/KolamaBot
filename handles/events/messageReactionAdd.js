8fc02e77bb4568ba913244dc4c7af8f14c616aec
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-17 19:41:07 +0100

    Update messageReactionAdd.js

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -4,18 +4,16 @@ module.exports = {
             console.log("i work")
 
             try {
-                //if (reaction.message.guild) {
+                if (reaction.message.guild) {
                     const messageId = reaction.message.id;
                     const reactionId = reaction.emoji.id;
                     const reactionName = reaction.emoji.name;
-                    //if (messageId == '1197237806486011926') {
-                        const member = reaction.message.guild.members.cache.get(user.id);
-                        if (!member) {
-                            return;
-                        }
-                        if (member) console.log(member.roles);
-                    //}
-                //}
+                    const member = reaction.message.guild.members.cache.get(user.id);
+                    if (!member) {
+                        return;
+                    }
+                    if (member) console.log(member.roles);
+                }
             }
             catch (error) {
                 console.log(error);
