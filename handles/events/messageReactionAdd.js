f4010579fada3aafbdac74f432a3e58e1f34b0e3
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-17 18:56:38 +0100

    Update messageReactionAdd.js

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -5,12 +5,17 @@ module.exports = {
         if (user.partial) {
             try {
                 await user.fetch();
+                if (reaction.message.guild) {
+                    if (reaction.message.id == '1197237806486011926') {
+                        const member = reaction.message.guild.members.cache.get(user.id);
+                    
+                        if (member) console.log(member.roles);
+                    }
+                }
             }
             catch (error) {
-                console.error('Erreur en récupérant le user : ', error);
-                return;
+                console.error(error);
             }
-            console.log(user.roles);
         }
     },
 };
