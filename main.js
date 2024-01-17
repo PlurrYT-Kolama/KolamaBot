005c1f67a0735505caf6bc40c59b27dd9afce8cd
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-17 19:59:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,19 +9,7 @@ if (process.env.test) {
     client.test = true
 }
 client.commands = new Collection()
-client.guilds.fetch('1093888852546043914') // Fetch the guild by ID
-  .then(guild => {
-    guild.channels.fetch('channel_id') // Replace 'channel_id' with your channel ID
-      .then(channel => {
-        channel.messages.fetch('1197237806486011926') // Fetch the message by ID
-          .then(message => {
-            client.ticketcache = message;
-          })
-          .catch(console.error); // Log any errors while fetching the message
-      })
-      .catch(console.error); // Log any errors while fetching the channel
-  })
-  .catch(console.error); // Log any errors while fetching the guildclient.commands = new Collection()
+
 const events = fs.readdirSync('./handles/events').filter(file => file.endsWith('.js'));
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
