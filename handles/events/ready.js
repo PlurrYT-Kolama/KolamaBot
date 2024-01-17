63e37d1382db20ad4ed02da6af4f6c370be9bb89
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-17 15:29:47 +0100

    Tired to add tickets

--- handles/events/ready.js
+++ handles/events/ready.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { ActivityType } = require('discord.js')
+const {ThreadManager} = require('discord-tickets');
 module.exports = {
     name: 'ready',
     once: true,
@@ -8,6 +9,13 @@ module.exports = {
         ascii = `   ____        _                        _       \n  | __ )  ___ | |_   _ __ ___  __ _  __| |_   _ \n  |  _ \\ / _ \\| __| | '__/ _ \\/ _' |/ _' | | | |\n  | |_) | (_) | |_  | | |  __/ (_| | (_| | |_| |\n  |____/ \\___/ \\__| |_|  \\___|\\__,_|\\__,_|\\__, |\n                                          |___/ `
         console.log(`Logged in as ${client.user.tag}\nThis bot is in ${sCount} servers\n${ascii}\n\n`)
         client.user.setActivity(`Kolama.net`, { type: ActivityType.Watching })
+        client.ticketManager = new ThreadManager(client, {
+            enabled: true,
+            channelId: "1094922348697763890",
+            staffRole: "1101223437659279360",
+            storage: `../../../tickets.json`,
+            ticketCache: true
+        });
         if (client.test) {
             process.exit(0)
         }
