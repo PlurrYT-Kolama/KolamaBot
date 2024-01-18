5f5c76e6e3d5171a12b175bb01e8657d29e9c018
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 21:06:37 +0100

    Update messageReactionAdd.js

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -3,19 +3,17 @@ module.exports = {
     once: false,
     async execute(reaction, user)  {
         try {
-                const message = !reaction.message.partial ? reaction.message : await reaction.message.fetch();
-                const memberId = await reaction.message.guild.members.fetch(user.id)
-                const guildId = reaction.message.guild.id;
-                if(reaction.message.id === '1197237806486011926'){
-                    console.log(reaction._emoji.name)
-                    if(reaction._emoji.name === ':support:'){
-                        await client.ticketManager.createTicket(guildId, memberId);
-                    }
+            const message = !reaction.message.partial ? reaction.message : await reaction.message.fetch();
+            const memberId = await reaction.message.guild.members.fetch(user.id);
+            const guildId = reaction.message.guild.id;
+            if(reaction.message.id === '1197237806486011926'){
+                console.log(reaction._emoji.name);
+                if(reaction._emoji.name === ':support:'){
+                    await client.ticketManager.createTicket(guildId, memberId);
                 }
-    
+            }
         } catch (error) {
             console.error('Something went wrong when fetching the message: ', error);
-            return;
-        };
-    };
+        }
+    }
 };
