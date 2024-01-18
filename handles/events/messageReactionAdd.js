9d949fbf34b06e5897a2ac6e3e89fcc6c53f9db6
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 21:17:59 +0100

    Update messageReactionAdd.js

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -6,12 +6,12 @@ module.exports = {
             const message = !reaction.message.partial ? reaction.message : await reaction.message.fetch();
             const memberId = await reaction.message.guild.members.fetch(user.id);
             const guildId = reaction.message.guild.id;
-            if(reaction.message.id === '1197237806486011926'){
+            //if(reaction.message.id === '1197237806486011926'){
                 console.log(reaction._emoji.name);
                 if(reaction._emoji.name === '🎟️'){
                     await client.ticketManager.createTicket(guildId, memberId);
                 }
-            }
+            //}
         } catch (error) {
             console.error('Something went wrong when fetching the message: ', error);
         }
