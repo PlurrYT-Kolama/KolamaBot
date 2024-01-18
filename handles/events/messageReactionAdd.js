0449058d9de4894884af1f8aa27e9e06a2d50782
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 21:05:07 +0100

    Add support ticket creation + format code

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -2,19 +2,20 @@ module.exports = {
     name: 'messageReactionAdd',
     once: false,
     async execute(reaction, user)  {
-    try {
-            const message = !reaction.message.partial ? reaction.message : await reaction.message.fetch();
-            const memberId = await reaction.message.guild.members.fetch(user.id)
-            const guildId = reaction.message.guild.id;
-            if(reaction.message.id === '1197237806486011926'){
-                console.log(reaction._emoji.name)
-                if(reaction._emoji.name === ':support:'){
-                    await client.ticketManager.createTicket(guildId, memberId);
+        try {
+                const message = !reaction.message.partial ? reaction.message : await reaction.message.fetch();
+                const memberId = await reaction.message.guild.members.fetch(user.id)
+                const guildId = reaction.message.guild.id;
+                if(reaction.message.id === '1197237806486011926'){
+                    console.log(reaction._emoji.name)
+                    if(reaction._emoji.name === ':support:'){
+                        await client.ticketManager.createTicket(guildId, memberId);
+                    }
                 }
-            }
-
-    } catch (error) {
-        console.error('Something went wrong when fetching the message: ', error);
-        return;
-    }
+    
+        } catch (error) {
+            console.error('Something went wrong when fetching the message: ', error);
+            return;
+        }
+    };
 };
