52c10d1c05c9783aa385e2268f52968927cb3a28
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 21:55:05 +0100

    Close ticket creation | Try #1

---
+++ commands/close.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require(`discord.js`);
+const { updatejson, checkjson, checkrole } = require('../jsonupdate.js'); //adjust path correctly
+let object = require('../times.json')
+const discordTranscripts = require('discord-html-transcripts');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("limit")
+        .setDescription("Check your commands limit")
+        .setDMPermission(true)
+    ,
+    async execute(interaction, client) {
+        let checkresult = await checkrole(interaction.member.roles.highest.id)
+        if (checkresult == true) {// check for staff YAY
+          if (client.ticketManager.tickets.get(interaction.channel.id);)
+            interaction.reply({content: `Closing ticket using staff access..`})
+            const channel = interaction.channel; // or however you get your TextChannel
+            const attachment = await discordTranscripts.createTranscript(channel);
+            const channel2 = await client.channels.fetch('1104136422304272524')
+            channel2.send({
+              files: [attachment],
+            });
+            const ticket = client.ticketManager.tickets.get(interaction.channel.id);
+            await client.ticketManager.closeTicket(ticket);
+          }
+        } else {
+            interaction.reply({content: `Please ping any avaliable staff from Trust & Safety Departament To help you close ticket!`})
+        }
+    },
+};
