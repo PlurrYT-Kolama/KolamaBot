0b19c45b96d678ef0b0e8918ae3589a9388021b0
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 20:34:34 +0100

    Update messageReactionAdd.js

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -21,9 +21,30 @@
     },
 }; */
 module.exports = {
-    name: 'MessageReactionAdd',
+    name: 'messageReactionAdd',
     once: false,
-    async execute(reaction, client) {
-        console.log("works")
+    async execute(reaction, user)  {
+            const member = reaction.message.guild.members.fetch(user.id)
+            if (reaction.partial) {
+                // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+                try {
+                    await reaction.fetch();
+                } catch (error) {
+                    console.error('Something went wrong when fetching the message: ', error);
+                    // Return as `reaction.message.author` may be undefined/null
+                    return;
+                }
+            }
+            // Now the message has been cached and is fully available
+            if(reaction.message.id === '1197237806486011926'){
+            //const role = reaction.message.guild.roles.cache.find(r => r.name === '🔔')
+            console.log(reaction._emoji.name)
+            if(reaction._emoji.name === '🔔'){
+                //member.roles.add(role.id)
+                console.log(test)
+            } /*else if(reaction.name === '🔎'){
+                us
+            }*/
+        //reaction.users.remove(user.id)
     },
 };
