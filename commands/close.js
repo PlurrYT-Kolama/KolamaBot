59d6b95e518a36f697c2ed90663f77f38dbba139
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 21:55:59 +0100

    Genius of the week error | Try #2

--- commands/close.js
+++ commands/close.js
@@ -12,7 +12,7 @@ module.exports = {
     async execute(interaction, client) {
         let checkresult = await checkrole(interaction.member.roles.highest.id)
         if (checkresult == true) {// check for staff YAY
-          if (client.ticketManager.tickets.get(interaction.channel.id);)
+          if (client.ticketManager.tickets.get(interaction.channel.id))
             interaction.reply({content: `Closing ticket using staff access..`})
             const channel = interaction.channel; // or however you get your TextChannel
             const attachment = await discordTranscripts.createTranscript(channel);
