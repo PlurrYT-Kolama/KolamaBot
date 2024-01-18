adf96fbddfa138284a19b14fc67b8d15568a2112
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 20:51:47 +0100

    Update handleEvents.js

--- handles/handleEvents.js
+++ handles/handleEvents.js
@@ -9,28 +9,4 @@ module.exports = (client) => {
             }
         }
     };
-    client.on('messageReactionAdd', async (reaction, user) => {
-                const member = reaction.message.guild.members.fetch(user.id)
-                if (reaction.partial) {
-                    try {
-                        await reaction.fetch();
-                    } catch (error) {
-                        console.error('Something went wrong when fetching the message: ', error);
-                        // Return as `reaction.message.author` may be undefined/null
-                        return;
-                    }
-                }
-                //if(reaction.message.id === '1197237806486011926'){
-                    //const role = reaction.message.guild.roles.cache.find(r => r.name === '🔔')
-                    console.log(reaction._emoji.name)
-                    if(reaction._emoji.name === '🔔'){
-                        //member.roles.add(role.id)
-                        console.log(test)
-                    }
-              //  }/*else if(reaction.name === '🔎'){
-                   // us
-                //}
-            //reaction.users.remove(user.id)
-           // }
-    })
 }
