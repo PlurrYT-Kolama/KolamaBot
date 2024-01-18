9026bdbe1b3f386c87f0d020941a9b772a3d69ea
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 21:20:35 +0100

    Update messageReactionAdd.js

--- handles/events/messageReactionAdd.js
+++ handles/events/messageReactionAdd.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'messageReactionAdd',
     once: false,
-    async execute(reaction, user, client)  {
+    async execute(reaction, user)  {
         try {
             const message = !reaction.message.partial ? reaction.message : await reaction.message.fetch();
             const memberId = await reaction.message.guild.members.fetch(user.id);
