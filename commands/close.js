1030486b426b9a1376e2592e7a7cf7b239db80d7
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-18 22:00:33 +0100

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -10,21 +10,21 @@ module.exports = {
         .setDMPermission(true)
     ,
     async execute(interaction, client) {
-    if (client.ticketManager.tickets.get(interaction.channel.id))
-        var checkresult = await checkrole(interaction.member.roles.highest.id)
-        if (checkresult == true) {// check for staff YAY
-            interaction.reply({content: `Closing ticket using staff access..`})
-            const channel = interaction.channel; // or however you get your TextChannel
-            const attachment = await discordTranscripts.createTranscript(channel);
-            const channel2 = await client.channels.fetch('1104136422304272524')
-            channel2.send({
-              files: [attachment],
-            });
-            const ticket = client.ticketManager.tickets.get(interaction.channel.id);
-            await client.ticketManager.closeTicket(ticket);
-        } else {
-            interaction.reply({content: `Please ping any avaliable staff from Trust & Safety Departament To help you close ticket!`})
+        if (client.ticketManager.tickets.get(interaction.channel.id))
+            var checkresult = await checkrole(interaction.member.roles.highest.id);
+            if (checkresult == true) {// check for staff YAY
+                interaction.reply({content: `Closing ticket using staff access..`})
+                const channel = interaction.channel; // or however you get your TextChannel
+                const attachment = await discordTranscripts.createTranscript(channel);
+                const channel2 = await client.channels.fetch('1104136422304272524');
+                channel2.send({
+                  files: [attachment],
+                });
+                const ticket = client.ticketManager.tickets.get(interaction.channel.id);
+                await client.ticketManager.closeTicket(ticket);
+            } else {
+                interaction.reply({content: `Please ping any avaliable staff from Trust & Safety Departament To help you close ticket!`})
+            }
         }
-    }
     },
 };
