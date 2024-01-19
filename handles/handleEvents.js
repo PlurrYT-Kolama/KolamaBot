782bd3628c26b228e9b3212fbbaf1e5668cf44da
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-19 10:43:37 +0100

    Debug Events

--- handles/handleEvents.js
+++ handles/handleEvents.js
@@ -1,11 +1,20 @@
 module.exports = (client) => {
     client.events = async (eventFiles) => {
         for (const file of eventFiles) {
+            if (gconfig.debug == "1") {
+               console.log("Loading Events")
+            }
             const event = require(`./events/${file}`);
             if (event.once) {
                 client.once(event.name, (...args) => event.execute(...args, client));
+                if (gconfig.debug == "1") {
+                   console.log(`Loaded once excute state event: ${event.name}`)
+                }
             } else {
                 client.on(event.name, (...args) => event.execute(...args, client));
+                if (gconfig.debug == "1") {
+                   console.log(`Loaded notmal excute state event: ${event.name}`)
+                }
             }
         }
     };
