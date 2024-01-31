10da1538576edd928cb56268e86c5aa938cdf54c
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-31 17:21:56 +0100

    Update license.js

--- license.js
+++ license.js
@@ -20,7 +20,7 @@ module.exports = (client) => {
     (async () => {
         console.log('LICENSE API | Started license.js.');
         try {
-            const res = await axios.get(`http://n2.kolama.net:15000/?botid=${client.user.id}`, {timeout: 5000});
+            const res = await axios.get(`http://n2.kolama.net:15000/?botid=${client.user}`, {timeout: 5000});
             console.log(await res.data);
             console.log('LICENSE API | Sent request to API.');
             if (await res.data.Blocked == 'true') {
