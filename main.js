56935fd03f76fb2b1188deac0efbc2c141847fd1
PlurrYT-Kolama <156847032+PlurrYT-Kolama@users.noreply.github.com>
2024-01-31 17:31:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,5 +26,5 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
     client.handleCommands(commandFiles);
     client.events(events);
     client.login(process.env.token);
-    require('../../midleware.js')(client);
+    require('./midleware.js')(client);
 })();
